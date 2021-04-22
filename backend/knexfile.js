b2ae0d88fc3a5e939e4e0aabab25b821dc8abf3d
gabriel <grocre@gmail.com>
2021-04-22 10:26:36 -0300

    knex init

---
+++ backend/knexfile.js
@@ -0,0 +1,47 @@
+require(dotenv).config;
+
+module.exports = {
+
+  development: {
+    client: process.env.DATABASE_CLIENT,
+    connection: {
+      database: process.env.DATABASE_NAME, 
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD, 
+      port: process.env.DATABASE_PORT
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
