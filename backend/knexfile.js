a089c9322e86c3848aa180aaee9447edc349fdd8
gabriel <grocre@gmail.com>
2021-04-22 11:44:37 -0300

    knexfile and migration -> tables

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,51 +1,18 @@
-require('dotenv').config;
-
+require('dotenv').config();
 module.exports = {
 
   development: {
     client: 'pg',
+
     connection: {
       database: process.env.DATABASE_NAME, 
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD, 
-      port: process.env.DATABASE_PORT
-    },
-    migrations: {
-      tableName: 'migrations',
-      directory: `${__dirname}/src/database/migrations` //caminho do arquivo que vai ser criado
+      user: process.env.DATABASE_USERNAME, 
+      password: process.env.DATABASE_PASSWORD,
+      port: 5432
     }, 
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "migrations", 
+      directory: `${__dirname}/src/database/migrations`
     }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
