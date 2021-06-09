e2f4c856ce2a17f97e9d7b1efcf728fa0051b3d9
gabriel <grocre@gmail.com>
2021-06-09 17:44:02 -0300

    Optimized db connection

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,7 +1,5 @@
-const knexfile = require('../../knexfile');
-const knex = require('knex')(knexfile['development']);
+const knexfile = require("../../knexfile");
+const knex = require("knex")(knexfile["development"]);
 
-
-// TECNOLOGIA MT AVANÇADA!!!
 module.exports = knex;
 
