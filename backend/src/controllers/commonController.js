4b1962f1d139ab7decd91ef7a3332d0f7dfd00bd
Thiago <thiagoandre2121@gmail.com>
2021-06-15 17:56:37 -0300

    Added userController

--- backend/src/controllers/commonController.js
+++ backend/src/controllers/commonController.js
@@ -2,34 +2,24 @@
 const database = require('../database');
 
 
-//json sempre minusculo -> sempre utilizando ponto e vírgula 
-module.exports = class commonController {
+//json sempre minusculo -> sempre utilizando ponto e vírgula
+module.exports = {
+
     async index(req, res){
         return res.json({
            "apresentação": "Como o ramo é",
            "noticias": "Pelo visto não vai ter",
            "patrocinadores": "Patrocínadores do Ramo"
         });
-    }
-    
-
-    /* O carrossel precisa de dinamismo na lógica do Back-end, a ideia é colocar um argumento obrigatório para conseguir realizar a troca das informações de acordo com a equipe escolhida no carrossel */ 
-
-    async equipe(req, res){
-        return res.json({
-            "equipes": "Byte, Botz, Rocket, WIE, Social, Power",
-            "premios": "premioByte, premioBotz, premioRocket"
-        });
-    }
+    },
 
     async projetos(req, res){
         return res.json({
             "projetos": "IA, Console, Jogos, Web",
             "imgs": "IAImg, ConsoleImg, JogosImg, WebImg"
         });
-    }
-    
-    
+    },
+
     /* A ideia é que após o encerramento do PSE, seja enviado automaticamente uma planilha com as pessoas inscritas para o email do Ramo (lib tipo multer) */
 
     async pse(req,res){
@@ -38,23 +28,23 @@ module.exports = class commonController {
             "equipesDisp": "byte, botz, rocket",
             "areasDisp": "prog, abc, bca"
         });
-    }
+    },
 
     async noticias(req, res){
         return res.json({
             "noticias": "Julio foi expulso do ramo!",
             "img": "imgDaNoticia"
         });
-    }
+    },
 
     async noticia(req, res){
         return res.json({
-            "cabeçalho": "nome", 
-            "corpo": "corpo da notícia", 
+            "cabeçalho": "nome",
+            "corpo": "corpo da notícia",
             "link": "se tiver link do instagram relacionado a noticia"
         });
-    }
-    
+    },
+
     async sobre(req,res){
         return res.json({
             "imagem": "http://www.youtube.com/",
@@ -63,4 +53,3 @@ module.exports = class commonController {
         });
     }
 }
-    
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,33 @@
 const knex = require('../database');
+const {v4} = require('uuid');
 
 module.exports = {
     async createUser(req, res){
         const {name, email, password, role} = req.body;
-        await knex('users').insert({
 
-            name, 
-            email,
-            password, 
-            role
-        })
-        res.send();
+        try {
+            await knex('users').insert({
+                id: v4(),
+                name,
+                email,
+                password,
+                role
+            });
+            return res
+                .status(200)
+                .json({
+                   "message": "Usuário Cadastrado"
+                });
+        } catch (err) {
+            return res
+                .status(422)
+                .json({"message": "Usuário já cadastrado"}); //melhorar a lógica para usuário já cadastrado
+        }
+    },
+
+    async index(req, res){
+        const users = await knex('users').select('*');
+        return res
+            .json({'users': users});
     }
 }
