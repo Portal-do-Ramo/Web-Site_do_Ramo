7f104c329a208d04d043f7e35438721c4c64c331
Thiago <thiagoandre2121@gmail.com>
2021-06-15 17:54:42 -0300

    Added crew and users routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const commonController = require('./controllers/commonController');
-const userController = require('./controllers/userController');
-
 const router = express.Router();
 
+const commonController = require('./controllers/commonController.js');
+const crewController = require('./controllers/crewController.js');
+const userController = require('./controllers/userController.js');
+
+
 router
-    .get("/", commonController.index)
-    .post("/", userController.createUser);
+    .get('/', commonController.index)
+    .get('/crew', crewController.index)
+	.get('/users', userController.index)
+    .post('/users', userController.createUser)
 
 module.exports = router;
