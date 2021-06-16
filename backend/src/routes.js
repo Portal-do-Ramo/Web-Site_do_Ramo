f7ac67cb4ea02db98e10ac13097e85719dbce9d9
gabriel <grocre@gmail.com>
2021-06-16 20:58:23 -0300

    Added new Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const commonController = require('./controllers/commonController.js');
-const crewController = require('./controllers/crewController.js');
-const userController = require('./controllers/userController.js');
-
+const commonController = require("./controllers/commonController.js");
+const crewController = require("./controllers/crewController.js");
+const userController = require("./controllers/userController.js");
+const projectController = require("./controllers/projectController.js");
+const awardController = require("./controllers/awardController")
 
 router
-    .get('/', commonController.index)
-    .get('/crew', crewController.index)
-	.get('/users', userController.index)
-    .post('/users', userController.createUser)
+    .get("/", commonController.index)
+    .get("/crews", crewController.index)
+	.get("/users", userController.index)
+	.get("/projects", projectController.index)
+    .get("/awards", awardController.index)
+    .get("/award/:id", awardController.show)
+    .post("/users", userController.createUser)
+	.post("/project", projectController.create)
+    .post("/award", awardController.create)
+	.delete("/crew", crewController.delete)
+    .delete("/project", projectController.delete)
+    .delete("/award", awardController.delete)
 
 module.exports = router;
