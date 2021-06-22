13b94eb218a3cb27ed523e394c70765559a51cd4
Thiago <thiagoandre2121@gmail.com>
2021-06-22 20:35:34 -0300

    Updated Project Migration

--- backend/src/database/migrations/20210609193511_create_projects.js
+++ backend/src/database/migrations/20210609193511_create_projects.js
@@ -5,10 +5,12 @@ exports.up = knex => {
       table.string("description").notNullable();
       table.string("image").notNullable();
       table.string("members").notNullable();
+      table.timestamp("beginning").notNullable();
+      table.timestamp("ended");
+      table.string("status").notNullable();
       table.timestamps(true, true);
       table.uuid("crew_id").notNullable().references("id").inTable("crews").onDelete("SET NULL");
     });
   };
   
 exports.down = knex => knex.schema.dropTable("projects");
-
