01e4a373f202ae3ecd5931eef7df70317fe56e4f
macpedro12 <pedroamorim62@gmail.com>
2021-06-23 09:37:07 -0300

    adicionando pasta front

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import styles from './styles.module.scss'
+import Link from 'next/link'
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/Ramo_Branco 2.png" alt="logo" className={styles.logo} />
+
+      <ul>
+        <li>Ramo Estudantil Cefet-RJ</li>
+        <li><p>Desenvolvendo pessoas através de projetos</p></li>
+      </ul>
+      <div className={styles.link}>
+        <Link href="">
+          <a>Sobre</a>
+        </Link>
+        <Link href="">
+          <a>Equipes</a>
+        </Link>
+        <Link href="">
+          <a>Notícias</a>
+        </Link>
+        <Link href="">
+          <a>Processo Seletivo</a>
+        </Link>
+        <Link href="">
+          <a>Parcerias</a>
+        </Link>
+        <Link href="">
+          <a>Contato</a>
+        </Link>
+        <Link href="">
+          <a>IEEE</a>
+        </Link>
+      </div>
+      <div className={styles.divRet}>
+        <img src="/Rectangle.png" className={styles.ret} />
+      </div>
+    </header>
+  )
+}
---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header";
+import '../styles/global.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <main>
+        <Header />
+      </main>
+    </div>
+  )
+}
+
+export default MyApp
