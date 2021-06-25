5759464a0c9209e087e320416f76889b22d70aef
macpedro12 <pedroamorim62@gmail.com>
2021-06-25 11:56:47 -0300

    Updating footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     <div className={styles.footer}>
       <div className={styles.ramo}>
         <img src="/Ramo_Branco 2.png" alt="logo" />
-        <h1>Ramo Estudantil </h1>
-        <h1>IEEE CEFET-RJ</h1>
+        <h1>Ramo Estudantil <br/> IEEE CEFET-RJ</h1>
+        
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
           odio elementum bibendum aliquam. Vel, tempor tincidunt enim,
@@ -21,18 +21,41 @@ export default function Footer() {
         </p>
       </div>
       <div className={styles.equipes}>
-        <img src="WieLogo.png" />
-        <img src="../../../WolfByte.png" />
-        <img src="SocialLogo.png" />
-        <img src="../../RocketLogo.png" />
-        <img src="../../PowerLogo.png" />
-        <img src="../../BotzLogo.png" />
+        <table>
+          <tr>
+            <img src="/wie.svg" />
+            <img src="/wolfbyte.svg" />
+          </tr>
+          <tr>
+            <img src="/Social.svg" />
+            <img src="/Rocket.svg" />
+          </tr>
+          <tr>
+            <img src="/Power.svg" />
+            <img src="/Botz.svg" />
+          </tr>
+          <tr>
+            <img src="/Gestao.svg"/>
+            <img src="/Marketing.svg"/>
+          </tr>
+        </table>
+        
       </div>
       <div className={styles.info}>
         <h1>Contatos </h1>
         <p>Av. Maracanã – Maracanã, Rio de Janeiro – RJ</p>
         <p>0000-0000</p>
         <p>iadobitcoin@gmail.com</p>
+        <a target="_blank" href="https://pt-br.facebook.com/ramocefet/">
+          <img src="/Facebook (2).svg"/>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/ramocefet/">
+          <img src="/Instagram (2).svg"/>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/company/ramocefet/">
+          <img src="/Linkedin (2).svg"/>
+        </a>
+
 
         <p>Desenvolvido por Ticoliro, Julioboladin2001,
         Thiaguin, Pedroza, Vinicin, Japa, Natanzon,
