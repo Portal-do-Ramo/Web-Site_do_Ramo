a2aecc702a187f895346e81d9828e15bad1168a0
Thiago <thiagoandre2121@gmail.com>
2021-06-25 00:57:48 -0300

    Header Improvement(navbar and curved border)

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 
 export default function Header() {
-  return (
+  return (<>
     <header className={styles.headerContainer}>
       <img src="/Ramo_Branco 2.png" alt="logo" className={styles.logo} />
 
@@ -10,32 +10,51 @@ export default function Header() {
         <li>Ramo Estudantil Cefet-RJ</li>
         <li><p>Desenvolvendo pessoas através de projetos</p></li>
       </ul>
-      <div className={styles.link}>
-        <Link href="">
-          <a>Sobre</a>
-        </Link>
-        <Link href="">
-          <a>Equipes</a>
-        </Link>
-        <Link href="">
-          <a>Notícias</a>
-        </Link>
-        <Link href="">
-          <a>Processo Seletivo</a>
-        </Link>
-        <Link href="">
-          <a>Parcerias</a>
-        </Link>
-        <Link href="">
-          <a>Contato</a>
-        </Link>
-        <Link href="">
-          <a>IEEE</a>
-        </Link>
-      </div>
+      <div className={styles.linkContainer}>
+        <div>
+          <Link href="">
+            <a>Sobre</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>Equipes</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>Notícias</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>Processo seletivo</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>Parcerias</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>Contato</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <a>IEEE</a>
+          </Link>
+        </div>
+        </div>
       <div className={styles.divRet}>
         <img src="/Rectangle.png" className={styles.ret} />
       </div>
+      
     </header>
+    
+    <img src="/curved-border.svg" width="100%"  />
+  
+  </>
   )
 }
