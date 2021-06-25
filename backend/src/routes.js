b1e05f3c4038c060f62c8cc8aa2b76df4d1da758
Vinícius Leitão <vinileitao2000@gmail.com>
2021-06-25 19:59:12 -0300

    Added new routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,7 @@ const awardController = require("./controllers/awardController");
 const depositionController = require("./controllers/depositionController");
 const newsController = require("./controllers/newsController");
 const sponsorController = require("./controllers/sponsorController");
+const roleController = require("./controllers/roleController");
 
 router
     .get("/", commonController.index)
@@ -18,32 +19,45 @@ router
 	.get("/depositions", depositionController.index)
 	.get("/news", newsController.index)
 	.get("/projects", projectController.index)
+	.get("/roles", roleController.index)
 	.get("/sponsors", sponsorController.index)
 	.get("/users", userController.index)
+
+
     .get("/award/:id", awardController.show)
 	.get("/crew/:id", crewController.show)
     .get("/news/:id", newsController.show)
     .get("/project/:id", projectController.show)
+	
+	
 	.patch("/award", awardController.update)
 	.patch("/crew", crewController.update)
     .patch("/deposition", depositionController.update)
 	.patch("/news", newsController.update)
 	.patch("/project", projectController.update)
+	.patch("/role", roleController.update)
 	.patch("/sponsor", sponsorController.update)
 	.patch("/user", userController.update)
+
+	
     .post("/award", awardController.create)
 	.post("/crew", crewController.create)
 	.post("/deposition", depositionController.create)
 	.post("/news", newsController.create)
 	.post("/project", projectController.create)
+	.post("/role", roleController.create)
 	.post("/sponsor", sponsorController.create)
     .post("/user", userController.create)
+
+
     .delete("/award", awardController.delete)
 	.delete("/crew", crewController.delete)
 	.delete("/deposition", depositionController.delete)
 	.delete("/news", newsController.delete)
     .delete("/project", projectController.delete)
+	.delete("/role", roleController.delete)
 	.delete("/sponsor", sponsorController.delete)
 	.delete("/user", userController.delete)
+
 
 module.exports = router;
