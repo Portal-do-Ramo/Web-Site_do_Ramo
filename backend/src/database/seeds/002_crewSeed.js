7ce1ddc0583c0963a5a24be97b655fe8897cbf6f
gabriel <grocre@gmail.com>
2021-06-25 15:34:04 -0300

    fixing some seeds

--- backend/src/database/seeds/002_crewSeed.js
+++ backend/src/database/seeds/002_crewSeed.js
@@ -7,14 +7,14 @@ exports.seed = function(knex) {
       
       return knex('crews').insert([
         
-        {id: v4(), name: "Gestão", about: "depressao e mt sofrimento"}, 
-        {id: v4(), name: "Marketing", about: "bo vender uma viagem pra um cara com o dinheiro do auxilio apenas kk"},
-        {id: v4(), name: "RocketWolf", about: "ovo lança fuguetin"},
-        {id: v4(), name: "SocialWolf", about:"ajuda uns velhinho"},
-        {id: v4(), name: "WIE", about: "Essa é braba"},
-        {id: v4(), name: "WolfBotz", about: "ih bo criar robo que briga kkk"},
-        {id: v4(), name: "WolfByte", about: "n ve a luz do sol"},
-        {id: v4(), name: "WolfPower", about: "vamo estoca vento"}
+        {id: v4(), name: "Gestão", about: "depressao e mt sofrimento", image: "www.sitedoramo.com/gestao"}, 
+        {id: v4(), name: "Marketing", about: "bo vender uma viagem pra um cara com o dinheiro do auxilio apenas kk", image: "www.sitedoramo.com/marketing"},
+        {id: v4(), name: "RocketWolf", about: "ovo lança fuguetin", image: "www.sitedoramo.com/rocketwolf"},
+        {id: v4(), name: "SocialWolf", about:"ajuda uns velhinho", image: "www.sitedoramo.com/socialwolf"},
+        {id: v4(), name: "WIE", about: "Essa é braba", image: "www.sitedoramo.com/wie"},
+        {id: v4(), name: "WolfBotz", about: "ih bo criar robo que briga kkk", image: "www.sitedoramo.com/wolfbotz"},
+        {id: v4(), name: "WolfByte", about: "n ve a luz do sol", image: "www.sitedoramo.com/wolfbyte"},
+        {id: v4(), name: "WolfPower", about: "vamo estoca vento", image: "www.sitedoramo.com/wolfpower"}
       ]);
     });
 };
