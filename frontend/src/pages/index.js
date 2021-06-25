efed38567b6bbc0756006e0860110ff89070ca3c
Thiago <thiagoandre2121@gmail.com>
2021-06-25 01:41:22 -0300

    Changed ourStory section to display flex

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -6,33 +6,37 @@ import styles from '../styles/index.module.scss';
 export default function Home() {
   return (
     <div id={styles.page_container}>
+
       <div id={styles.text_container}>
-        <h1>Nossa História</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        odio elementum bibendum aliquam. Vel, tempor tincidunt enim, 
-        eu. Ut non dui nulla massa viverra. Pellentesque id integer 
-        vulputate nulla sed in aenean. Pulvinar suspendisse vitae etiam 
-        sem natoque aliquam amet pulvinar velit. Aliquet tempor iaculis 
-        curabitur cursus libero. Cursus odio nunc ipsum ipsum nunc. 
-        Mauris mi fringilla nibh a. Cursus tincidunt fames faucibus id 
-        iaculis egestas malesuada dis sed. Aliquet vel eu in arcu, 
-        adipiscing adipiscing luctus ligula tellus.
-        Nullam ullamcorper. Ac vestibulum, posuere blandit mauris. 
-        Semper accumsan, arcu sit egestas phasellus in senectus etiam 
-        et. Scelerisque in a quisque mattis iaculis pellentesque bibendum 
-        bibendum diam. Imperdiet velit ultricies vestibulum aenean 
-        cursus iaculis eget. Lacus, aliquet tellus viverra volutpat aliquam 
-        mauris sed hendrerit cursus. Laoreet maecenas aenean facilisi 
-        rhoncus. A mi, faucibus eu tortor iaculis fringilla nullam sed 
-        platea. Et arcu sed auctor vel. Quis sagittis, eros dolor facilisi.
-        Mi massa urna, commodo in dignissim. Varius neque quam in </p>
+        <div id={styles.ourStory}>
+          <h1>Nossa História</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          odio elementum bibendum aliquam. Vel, tempor tincidunt enim, 
+          eu. Ut non dui nulla massa viverra. Pellentesque id integer 
+          vulputate nulla sed in aenean. Pulvinar suspendisse vitae etiam 
+          sem natoque aliquam amet pulvinar velit. Aliquet tempor iaculis 
+          curabitur cursus libero. Cursus odio nunc ipsum ipsum nunc. 
+          Mauris mi fringilla nibh a. Cursus tincidunt fames faucibus id 
+          iaculis egestas malesuada dis sed. Aliquet vel eu in arcu, 
+          adipiscing adipiscing luctus ligula tellus.
+          Nullam ullamcorper. Ac vestibulum, posuere blandit mauris. 
+          Semper accumsan, arcu sit egestas phasellus in senectus etiam 
+          et. Scelerisque in a quisque mattis iaculis pellentesque bibendum 
+          bibendum diam. Imperdiet velit ultricies vestibulum aenean 
+          cursus iaculis eget. Lacus, aliquet tellus viverra volutpat aliquam 
+          mauris sed hendrerit cursus. Laoreet maecenas aenean facilisi 
+          rhoncus. A mi, faucibus eu tortor iaculis fringilla nullam sed 
+          platea. Et arcu sed auctor vel. Quis sagittis, eros dolor facilisi.
+          Mi massa urna, commodo in dignissim. Varius neque quam in </p>
+        </div>
         <div id={styles.main_container}>
-          <img id={styles.icon} src='/seu_pai.svg' width={500} height={600}/>
+          <img id={styles.icon} src='/seu_pai.svg' width={400} height={500}/>
           <a href="#" id={styles.button}>
             <Image src='/right-arrow.png' width={50} height={50}/>
           </a>
         </div>
       </div>
+
       <div className={styles.crew_content}>
         <h3>Equipes</h3>
         <section>
