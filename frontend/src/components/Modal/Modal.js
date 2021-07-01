a9f5f0e0ff3559226be472ca84791e93dd3b3878
macpedro12 <pedroamorim62@gmail.com>
2021-07-01 13:00:25 -0300

    Updated modal

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import styles from "./Modal.module.scss";
 
 const Modal = ({ show, onClose, children, title }) => {
   const [isBrowser, setIsBrowser] = useState(false);
@@ -11,19 +12,20 @@ const Modal = ({ show, onClose, children, title }) => {
     onClose();
   };
   const modalContent = show ? (
-    <div>
-      {title}
-      {children}
-      <a onClick={handleCloseClick}> x </a>
+    <div className={styles.overlay}>
+      <div className={styles.body}>
+        {children}
+        <a onClick={handleCloseClick}> x </a>
+      </div>
     </div>
   ) : null;
   if (isBrowser) {
     return ReactDOM.createPortal(
       modalContent,
-      document.getElementById('modal')
+      document.getElementById("modal")
     );
   } else {
     return null;
   }
-}
+};
 export default Modal;
