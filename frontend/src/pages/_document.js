b7bb55821c8584aa3473d525f03075a7e029b4da
macpedro12 <pedroamorim62@gmail.com>
2021-07-02 12:15:53 -0300

    Updated imports

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,21 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    
-    render(){
-        return(
-            <Html>
-                <Head>
-                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700;800&display=swap" rel="stylesheet"/>
-                </Head>
-                <body>
-                    <Main/>
-                    <NextScript/>
-                    <div id="modal"></div>
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charset="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal"></div>
+        </body>
+      </Html>
+    );
+  }
 }
