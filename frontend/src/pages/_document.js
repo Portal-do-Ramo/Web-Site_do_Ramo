676f579523ce0fc621ec791e29a7af96b8d1a275
macpedro12 <pedroamorim62@gmail.com>
2021-07-02 12:13:15 -0300

    Added react-slick imports

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
+                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700;800&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
