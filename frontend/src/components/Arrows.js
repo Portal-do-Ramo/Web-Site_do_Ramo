fdc18c03ce7469b4476bf4fb4f24cf1efd830ea2
Thiago <thiagoandre2121@gmail.com>
2021-07-03 23:01:02 -0300

    Created Arrows Component

---
+++ frontend/src/components/Arrows.js
@@ -0,0 +1,33 @@
+import styles from "../styles/equipes.module.scss";
+
+export const NextArrow = ({ onClick }) => {
+    return (
+      <div className={styles.next} onClick={onClick}>
+        <img src="/Vector (1).svg" />
+      </div>
+    );
+  };
+
+  export const PrevArrow = ({ onClick }) => {
+    return (
+      <div className={styles.prev} onClick={onClick}>
+        <img src="/Vector (2).svg" />
+      </div>
+    );
+  };
+
+export const ProjectNextArrow = ({ onClick }) => {
+    return (
+      <div className={styles.pnext} onClick={onClick}>
+        <img src="/Arrow_EquipesRight.svg" />
+      </div>
+    );
+  };
+
+export const ProjectPrevArrow = ({ onClick }) => {
+    return (
+      <div className={styles.pprev} onClick={onClick}>
+        <img src="/Arrow_EquipesLeft.svg" />
+      </div>
+    );
+  };
