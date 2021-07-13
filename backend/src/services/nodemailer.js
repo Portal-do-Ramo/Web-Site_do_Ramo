191ed8965915b01aa854261bf9064c11deeb4a23
gabriel <grocre@gmail.com>
2021-07-13 20:37:17 -0300

    Some validation changes -> still not working

--- backend/src/services/nodemailer.js
+++ backend/src/services/nodemailer.js
@@ -1,36 +1,37 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
+
+let transporter = nodemailer.createTransport({
+	host: "smtp.gmail.com",
+	port: 567,
+	secure: false,
+	auth: {
+		type: 'login',
+		user: process.env.EMAIL,
+		pass: process.env.SENHA
+	},
+	tls: {
+		rejectUnauthorized: false,
+	},
+});
+		
+//mexer depois
 async function main(){
-	let testAcount = await nodemailer.createTestAccount();
-
-
-	let transporter = nodemailer.createTransport({
-		host: "smtp.ethereal.email",
-		port: 587, 
-		secure: false,
-		auth: {
-			user: testAcount.user,
-			pass: testAcount.pass
-		}
-	});
-
-
 	let info = await transporter.sendMail({
-		from: '"Sei lá, mano" <foo@example.com>',  
-		to: "",
-		subject: "Olá mundo", 
+		from: '"Sei lá, mano" <foo@example.com>',
+		to: ["grocre@gmail.com"],
+		subject: "Olá mundo",
 		text: "Hello world",
 		html: "<b>Hello world</b>"
 	});
-
-	console.log("Message sent: %s", info.messageId);
-	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+	console.log(info);
 }
-
-/* dei uma olhada na lib
-* Tentei mandar uma mensagem pro meu email mas o gmail bloqueia ela 
+	
+main()
+	/* dei uma olhada na lib
+	
+	* Tentei mandar uma mensagem pro meu email mas o gmail bloqueia ela 
 * Tem um link aqui que eu acho que vai ser interessante 
 * https://nodemailer.com/usage/using-gmail/
 */
-
-main().catch(console.error);
