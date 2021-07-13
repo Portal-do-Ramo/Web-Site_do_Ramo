df87e7760572defbb1af5a32cc948ec3942be284
gabriel <grocre@gmail.com>
2021-07-13 20:35:41 -0300

    Joi validation and Bcrypt methods

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,9 @@
-const knex = require('../database');
-const {v4} = require('uuid');
-const authenticate = require('../services/authentication');
+const bcrypt = require("bcrypt");
+const {v4} = require("uuid");
+
+const knex = require("../database");
+const authenticate = require("../services/authentication");
+const Joi = require("../services/validation");
 
 module.exports = {
     
@@ -11,26 +14,38 @@ module.exports = {
 
     async create(req, res){
         const {name, email, password, role} = req.body;
-		
-    
-        try {
-            await knex('users').insert({
-                id: v4(),
-                name,
-                email,
-                password,
-                role
-            });
 
-            return res
-                .status(201)
-                .json({
-                   "message": "Usuário Cadastrado", 
+        if(await knex('users').where({email}).first()){
+            return res.status(409).send({error:"User already exists"})
+        }
+
+		
+        try {
+			const {error} = Joi(req.body);
+            if(error == null){
+				
+				const hash = await bcrypt.hash(password, 10)
+				
+				//tranformar -> senha
+                await knex('users').insert({
+                    id: v4(),
+                    name,
+                    email,
+                    password: hash,
+                    role
                 });
+
+                return res
+                    .status(201)
+                    .json({
+                    "message": "Usuário Cadastrado", 
+                    });
+            }
+			return res.status(406).json({"message": error.message});
         } catch (err) {
             return res
                 .status(422)
-                .json({"message": "Usuário já cadastrado"}); //melhorar a lógica para usuário já cadastrado
+                .json({"message": err.message}); //melhorar a lógica para usuário já cadastrado
         }
     },
 
@@ -65,7 +80,8 @@ module.exports = {
 			if(!user.email) { 	// Verificar os status code.
 				return res.status(401).json({"message": "Email não existe"});
 			}
-			if(password !== user.password){ // implementar lógica do bcrypt
+
+			if(!await bcrypt.compare(password, user.password)){ // implementar lógica do bcrypt
 				return res.status(401).json({"message": "Senha inválida"});
 			}
 			
