d2455624aaa915a375aa4cbb1a032ad399aa5664
macpedro12 <pedroamorim62@gmail.com>
2021-07-15 17:10:01 -0300

    Added SearchBar component

---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+
+import styles from './styles.module.scss'
+
+export default function SearchBar ({searchQuery , setSearchQuery }){
+  
+  return(
+    <div>
+      <form  className={styles.form}>
+        <input type="text" value={searchQuery} onChange={(event)=>setSearchQuery(event.target.value)}/>
+        <img src="search.svg"/>
+      </form>
+    </div>
+
+  )
+}
