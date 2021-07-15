ec3af7fdb7977d8afb26653099390aaa2f68e7cd
macpedro12 <pedroamorim62@gmail.com>
2021-07-15 17:34:35 -0300

    Added Header

--- frontend/src/pages/noticias.js
+++ frontend/src/pages/noticias.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import SearchBar from "../components/SearchBar/SearchBar";
 import { useEffect, useState } from "react";
 import { convertDurationToTimeString } from "../utils/timeToString";
+import Header from "../components/Header/Header";
 
 const noticias = [
   {
@@ -168,100 +169,106 @@ export default function Noticias() {
   const tamanho = noticias.length;
 
   return (
-    <div className={styles.all}>
-      <div className={styles.recente}>
-        <div className={styles.jornal}>
-          <h1>Jornal do Ramo</h1>
-        </div>
-        <div className={styles.grid}>
-          {noticiasAtuais.map((noticias, idx) => (
-            <div>
-              {idx == 0 ? (
-                <div className={styles.primeira}>
-                  <img src={noticias.img} />
-                  <div className={styles.info}>
-                    <h1>{noticias.title}</h1>
-                    <p>{noticias.description}</p>
-                  </div>
-                </div>
-              ) : (
-                <div className={styles.noticiasRecentes}>
-                  <img src={noticias.img} />
-                  <div className={styles.info}>
-                    <h1>{noticias.title}</h1>
-                    <p>{noticias.description}</p>
-                  </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-        <div className={styles.mais}>
-          <span>Mais Recentes</span>
-        </div>
-        <div className={styles.triangulo}>
-          <img src="/Triangulo.svg" />
-        </div>
-      </div>
-      <div className={styles.half}>
-        <div className={styles.antigas}>
-          <h1>Notícias</h1>
-          <div className={styles.searchbar}>
-            <SearchBar
-              searchQuery={searchQuery}
-              setSearchQuery={setSearchQuery}
-            />
+    <div>
+      <Header />
+      <div className={styles.all}>
+        <div className={styles.recente}>
+          <div className={styles.jornal}>
+            <h1>Jornal do Ramo</h1>
           </div>
-          <div className={styles.total}>
-            {filteredNot.map((noticias) => (
-              <div className={styles.noticias}>
-                <table>
-                  <tr>
-                    <td>
-                      <img src={noticias.img} />
-                    </td>
-                    <td>
+          <div className={styles.grid}>
+            {noticiasAtuais.map((noticias, idx) => (
+              <div>
+                {idx == 0 ? (
+                  <div className={styles.primeira}>
+                    <img src={noticias.img} />
+                    <div className={styles.info}>
                       <h1>{noticias.title}</h1>
                       <p>{noticias.description}</p>
-                      <p className={styles.time}>
-                        {" "}
-                        Há {convertDurationToTimeString(noticias.time)}{" "}
-                      </p>
-                    </td>
-                  </tr>
-                </table>
+                    </div>
+                  </div>
+                ) : (
+                  <div className={styles.noticiasRecentes}>
+                    <img src={noticias.img} />
+                    <div className={styles.info}>
+                      <h1>{noticias.title}</h1>
+                      <p>{noticias.description}</p>
+                    </div>
+                  </div>
+                )}
               </div>
             ))}
           </div>
-          <div>
-            {click < tamanho / 5 - 1 ? (
-              <button
-                className={styles.button}
-                onClick={() => setClick(click + 1)}
-              >
-                Veja Mais
-              </button>
-            ) : null}
-            {click > 1 ? (
-              <button
-                className={styles.button2}
-                onClick={() => setClick(click - 1)}
-              >
-                -
-              </button>
-            ) : null}
+          <div className={styles.mais}>
+            <span>Mais Recentes</span>
+          </div>
+          <div className={styles.triangulo}>
+            <img src="/Triangulo.svg" />
           </div>
         </div>
-        <div className={styles.social}>
-          <a target="_blank" href="https://pt-br.facebook.com/ramocefet/">
-            <img src="/Facebook.svg" />
-          </a>
-          <a target="_blank" href="https://www.instagram.com/ramocefet/">
-            <img src="/Instagram.svg" />
-          </a>
-          <a target="_blank" href="https://www.linkedin.com/company/ramocefet/">
-            <img src="/Linkedin.svg" />
-          </a>
+        <div className={styles.half}>
+          <div className={styles.antigas}>
+            <h1>Notícias</h1>
+            <div className={styles.searchbar}>
+              <SearchBar
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+              />
+            </div>
+            <div className={styles.total}>
+              {filteredNot.map((noticias) => (
+                <div className={styles.noticias}>
+                  <table>
+                    <tr>
+                      <td>
+                        <img src={noticias.img} />
+                      </td>
+                      <td>
+                        <h1>{noticias.title}</h1>
+                        <p>{noticias.description}</p>
+                        <p className={styles.time}>
+                          {" "}
+                          Há {convertDurationToTimeString(noticias.time)}{" "}
+                        </p>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              ))}
+            </div>
+            <div>
+              {click < tamanho / 5 - 1 ? (
+                <button
+                  className={styles.button}
+                  onClick={() => setClick(click + 1)}
+                >
+                  Veja Mais
+                </button>
+              ) : null}
+              {click > 1 ? (
+                <button
+                  className={styles.button2}
+                  onClick={() => setClick(click - 1)}
+                >
+                  -
+                </button>
+              ) : null}
+            </div>
+          </div>
+          <div className={styles.social}>
+            <a target="_blank" href="https://pt-br.facebook.com/ramocefet/">
+              <img src="/Facebook.svg" />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/ramocefet/">
+              <img src="/Instagram.svg" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/company/ramocefet/"
+            >
+              <img src="/Linkedin.svg" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
