a0133b1d25851f0d3a39c05d001760712ac612ba
Thiago <thiagoandre2121@gmail.com>
2021-07-16 20:43:58 -0300

    Added Oath2 and mailer functions

--- backend/src/services/nodemailer.js
+++ backend/src/services/nodemailer.js
@@ -1,37 +1,80 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
+const { google } = require('googleapis')
+require('dotenv').config()
 
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
-let transporter = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 567,
-	secure: false,
-	auth: {
-		type: 'login',
-		user: process.env.EMAIL,
-		pass: process.env.SENHA
-	},
-	tls: {
-		rejectUnauthorized: false,
-	},
-});
-		
-//mexer depois
-async function main(){
-	let info = await transporter.sendMail({
-		from: '"Sei lá, mano" <foo@example.com>',
-		to: ["grocre@gmail.com"],
-		subject: "Olá mundo",
-		text: "Hello world",
-		html: "<b>Hello world</b>"
-	});
-	console.log(info);
+async function main(whichEmail, ...info) {
+    const accessToken = await oAuth2Client.getAccessToken()
+
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: process.env.SENDER_EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken
+      },
+    });
+
+    //mandar email de dúvidas
+    if (whichEmail === "email_duvidas") {
+      try {
+        let sendDoubts = await transporter.sendMail({
+          from: `${info.name} <${process.env.SENDER_EMAIL}>`,
+          to: `${process.env.GP_EMAIL}`,
+          subject: "Dúvida",
+          text: `${info.text}`,
+          html: '<h1>Olá</h1>',
+        });
+		    console.log(sendDoubts);
+      } catch {
+        return false;
+      }
+    };
+
+    // mandar arquivos csv
+    if (whichEmail === "email_arquivos") {
+      try {
+        let sendCSV = await transporter.sendMail({
+          from: `"Ramo Estudantil IEEE CEFET-RJ" <${process.env.SENDER_EMAIL}>`,
+          to: `${process.env.GP_EMAIL}`, 
+          subject: "Planilha de inscritos PSE",
+          text: 'lorem', // Colocar texto explicando o conteúdo do email 
+          html: '<h1>Olá</h1>',
+          attachments: 
+              [
+            {
+              filename: 'text3.txt',
+              //ver o path do csv e pesquisar algumas outras coisas sobre deleção e criaçaõ de CSVs
+              path: '/path/to/file.txt' 
+            },
+          ]
+        })
+      } catch {
+        return false
+      }
+    };
+
+    if (whichEmail === "email_confirmacao") {
+      try {
+        let confirmation = await transporter.sendMail({
+          from: `"Ramo Estudantil IEEE CEFET-RJ" <${process.env.SENDER_EMAIL}>`,
+          to: `${info.email}`,
+          subject: "Confirmação de inscrição de no Site do Ramo",
+              // Colocar texto explicando o conteúdo do email
+          text: `${text}`,
+          html: '<h1>Olá</h1>',
+        });
+      } catch {
+        return false;
+      }
+    };
 }
-	
-main()
-	/* dei uma olhada na lib
-	
-	* Tentei mandar uma mensagem pro meu email mas o gmail bloqueia ela 
-* Tem um link aqui que eu acho que vai ser interessante 
-* https://nodemailer.com/usage/using-gmail/
-*/
+
+module.exports = main;
