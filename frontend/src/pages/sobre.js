79b5473e913ea6a02faef105ea9b05e931cf9939
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-07-17 22:32:23 -0300

    Finished static about page

--- frontend/src/pages/sobre.js
+++ frontend/src/pages/sobre.js
@@ -1,8 +1,42 @@
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import Value from '../components/Value/Value'
 import styles from "../styles/sobre.module.scss";
 
 export default function Sobre(){
+
+    let values = [
+        {
+            img: "/valor 1.svg",
+            title: "Trabalho em equipe",
+            description: "Trabalhar de maneira comunicativa e integrada prezando pelo respeito para agir em prol de um objetivo em comum."
+        },
+        {
+            img: "/valor 2.svg",
+            title: "Pensamento disruptivo",
+            description: "Pensar de maneira inovadora e criativa com objetivo de desenvolver projetos técnicos e/ou sociais para impactar positivamente as pessoas a sua volta."
+        },
+        {
+            img: "/valor 3.svg",
+            title: "Aprendizagem evolutiva",
+            description: "Capacitar os membros para que estes repassem o conhecimento de forma atenciosa e com segurança."
+        },
+        {
+            img: "/valor 4.svg",
+            title: "Empatia",
+            description: "Prezamos a diversidade de pessoas e projetos com a responsabilidade de manter um ambiente confortável, acolhedor e divertido para todo."
+        },
+        {
+            img: "/valor 5.svg",
+            title: "Orgulho IEEE",
+            description: "Sentimento universal de pertencimento no qual os membros prezam de forma apaixonada pelo desenvolvimento da extensão."
+        },
+        {
+            img: "/valor 6.svg",
+            title: "Espírito de liderança",
+            description: "Desenvolver, influenciar e estimular a inteligência emocional de nossos membros, formando novos líderes."
+        }
+    ]
 
     return (
         <>
@@ -11,9 +45,131 @@ export default function Sobre(){
             <main className={styles.main}>
                 <div className={styles.banner}></div>
                 
-                <div className={styles.about}>
+                <section className={styles.about}>
+                    <div className={styles.aboutContainer}>
+                        <section className={styles.aboutLeftContainer}>
+                            <article>
+                            <div className={styles.historyArrow}></div>
+                                <h1>O que é o Ramo ?</h1>
+                                <p>     Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Massa tempor, eu aenean 
+                                nisi, tortor. Tellus, posuere diam nunc ac ut 
+                                iaculis. Pharetra odio id netus eu a, 
+                                volutpat porttitor urna. Ac gravida in ac 
+                                viverra nibh. Nulla lectus amet, vel mattis. 
+                                Mauris, vel et massa netus sollicitudin 
+                                ligula. Ipsum aliquam a, sed nisi, mauris 
+                                eu risus ut. Ullamcorper libero, lectus 
+                                eleifend tincidunt viverra adipiscing 
+                                facilisis sed luctus. Odio sed elit, duis 
+                                vulputate nunc arcu magna elit. IEEE 
+                                nulla nisl sed morbi lorem. Malesuada 
+                                viverra consectetur pulvinar auctor. 
+                                Consequat, quis est fermentum parturient 
+                                proin tristique augue turpis. A amet, 
+                                eleifend libero tincidunt at. Praesent 
+                                vestibulum sollicitudin ultrices viverra.
+                                </p>
+                            </article>
 
-                </div>
+                            <article>
+                                <div className={styles.familyArrow}></div>
+                                <h2>Nossa família</h2>
+                                <div className={styles.familyPicture}><img src="/galerinha_ramo.png"/></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Massa tempor, eu aenean 
+                                nisi, tortor. Tellus, posuere diam nunc ac ut 
+                                iaculis. Pharetra odio id netus eu a, 
+                                volutpat porttitor urna. Ac gravida in ac 
+                                viverra nibh. Nulla lectus amet, vel mattis. 
+                                Mauris, vel et massa netus sollicitudin 
+                                ligula. Ipsum aliquam a, sed nisi, mauris 
+                                eu risus ut. Ullamcorper libero, lectus 
+                                eleifend tincidunt viverra adipiscing 
+                                facilisis sed luctus.</p> 
+
+                            </article>
+                        </section>
+                         
+                        <section className={styles.aboutRightContainer}>
+                            <article>
+                                
+                                <h2>História</h2>
+                                <p>Criado em 2004, o Ramo Estudantil IEEE CEFET RJ
+                                não deu muito certo, porém, em 2016 dois membros 
+                                reabriram as portas, formando uma pequena equipe
+                                focada em programação e com o intuito apenas
+                                de participar do IEEExtreme.</p>
+                                    
+                                <p>Ainda em 2016, pela primeira vez, o Ramo teve
+                                o seu Processo Seletivo Externo. Crescendo aos
+                                poucos e tomando forma de extensão, o Ramo era 
+                                dividido em pequenos projetos. Agora com uma 
+                                nova visão, regras e um estatuto a ser implementado
+                                passou a assemelhar-se mais a uma extensão.</p>
+
+                                <p>Em 2017, a presidência começou a dividir as equipes
+                                e neste mandato consolidou-se a Wolfbyte, WolfBotz,
+                                Wolfpower, SocialWolf e RocketWolf, com poucos 
+                                projetos e bem básicos.</p>
+
+                                <p>Em 2018, foram criadas a equipe de diretoria e projetos
+                                fazendo com que a gente atingisse um crescimento 
+                                enorme, nos permitindo ao chegar ao que somos hoje.</p>
+                                    
+                                <p>Até hoje, inúmeros alunos do CEFET-RJ passaram pelo
+                                Ramo e inúmeros alunos ainda vão passar, dizem que as
+                                pessoas que conheceram o Ramo, nunca o esquece. </p>
+
+                                <p>Continuar desenvolvendo pessoas através de projetos
+                                técnicos e sociais é a nossa missão e é isso o que nos move
+                                a continuar.</p>
+                                
+                            </article>
+                        </section>
+
+                    </div>
+                </section>
+
+                <section className={styles.ourValues}>
+                    <h2>Nossos valores</h2>
+
+                    <section className={styles.valuesContainer}>
+                        <Value props={values[0]}/>
+                        <Value props={values[1]}/>
+                        <Value props={values[2]}/>
+                        <Value props={values[3]}/>
+                        <Value props={values[4]}/>
+                        <Value props={values[5]}/>
+                    </section>
+                    
+                </section>
+
+                <section className={styles.contactUs}>
+                    <h2>Fale conosco</h2>
+
+                    <div className={styles.contactUsContent}>
+
+                        <section className={styles.contactUsImg}>
+                            <img src="/seu_pai.svg"/>
+                        </section>
+
+                        <section className={styles.contactUsForm}>
+                            <div className={styles.triangleForm}></div>
+                            <form>
+                                <label for="nameField">Nome</label>
+                                <input id="nameField" type="text"/>
+                                <label for="emailField">E-mail</label>
+                                <input id="emailField" type="email"/>
+                                <label for="descriptionField">Descrição</label>
+                                <textarea id="descriptionField"/>
+                                <button type="button">Enviar</button>
+                            </form>
+                        </section>
+                        
+                    </div>
+                </section>
+
             </main>
             
             <Footer/>
