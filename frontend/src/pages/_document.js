539182119ff57160029e04a92df44f6131f63db1
macpedro12 <pedroamorim62@gmail.com>
2021-08-05 08:52:09 -0300

    Added Roboto font

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -20,9 +20,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700;800&display=swap"
             rel="stylesheet"
           />
-          <link 
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" 
-          rel="stylesheet"
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
