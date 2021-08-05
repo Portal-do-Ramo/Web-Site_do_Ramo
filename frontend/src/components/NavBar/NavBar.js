dfc75c06c977477294a65456fbc767895f03fee5
macpedro12 <pedroamorim62@gmail.com>
2021-08-05 08:50:41 -0300

    Added NavBar component for Admin page

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+const edit = [
+  {
+    id: 1,
+    title: "Blog",
+  },
+  {
+    id: 2,
+    title: "Equipes",
+  },
+  {
+    id: 3,
+    title: "Projetos",
+  },
+];
+
+export default function NavBar() {
+  return (
+    <div className={styles.all}>
+      <div className={styles.title}>
+        <Image src="/Ramo_logo.svg" width={94} height={93} />
+        <div className={styles.title2}>
+          <p>Admin</p>
+          <p>Site do Ramo</p>
+        </div>
+      </div>
+      <div className={styles.edit}>
+        {edit.map((edit) => (
+          <div key={edit.id}>
+            <h1>{edit.title}</h1>
+            <div className={styles.options}>
+              <Link href="/">
+                <a>
+                  <p>Cadastrar</p>
+                </a>
+              </Link>
+              <Link href="/">
+                <a>
+                  <p>Gerenciar</p>
+                </a>
+              </Link>
+            </div>
+          </div>
+        ))}
+        <h1>PSE</h1>
+        <Link href="/">
+          <a>
+            <p className={styles.options}>Editar</p>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
