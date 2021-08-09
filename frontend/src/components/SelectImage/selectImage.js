91c91b895a970e11e5696e16013d78c5e83559b9
Thiago <thiagoandre2121@gmail.com>
2021-08-08 21:59:47 -0300

    Fixed select images component

--- frontend/src/components/SelectImage/selectImage.js
+++ frontend/src/components/SelectImage/selectImage.js
@@ -1,16 +1,40 @@
-import styles from './selectImage.module.scss'
+import styles from './SelectImage.module.scss'
+import {useState} from "react";
 
 export default function MktImageSection() {
+    const [images, setImages] = useState([])
+    const [previewImages, setPreviewImages] = useState([])
+    const imagesArray = []
+
+    function handleSelectImages(event) {
+        if(!event.target.files) return
+        
+        const selectedImages = Array.from(event.target.files)
+        setImages(selectedImages)
+        const selectedImagesPreview = selectedImages.map(image => {
+            return URL.createObjectURL(image)
+        })
+        imagesArray.push()
+        setPreviewImages(selectedImagesPreview)
+    }
 
     return(
         <div className={styles.imageCard}>
-            <label htmlFor="image[]" className={styles.newImage}>
-                <div className={styles.cross1}></div>
-                <div className={styles.cross2}></div>
-            </label>
-
+            {previewImages.length == 0 ? (
+                 <label htmlFor="image[]" className={styles.newImage}>
+                 <div className={styles.cross1}></div>
+                 <div className={styles.cross2}></div>
+             </label>
+            ) : 
+            previewImages.map(image => {
+                return (
+                    <img className={styles.image} key={image} src={image} />
+                )
+            }
+            )}
+           
             {/* input escondido */}
-            <input multiple onChange={handleSelectImages} type="file" id="image[]"  />
+            <input onChange={handleSelectImages} type="file" id="image[]"  />
             <p>capa</p>
         </div>
     )
