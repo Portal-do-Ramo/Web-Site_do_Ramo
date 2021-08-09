28a4799e14c09b93b6ae078c06c78784476d6f27
Thiago <thiagoandre2121@gmail.com>
2021-08-09 12:48:27 -0300

    Small changes SearchBar

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -4,12 +4,9 @@ import styles from './styles.module.scss'
 export default function SearchBar ({searchQuery , setSearchQuery }){
   
   return(
-    <div>
       <form  className={styles.form}>
         <input type="text" value={searchQuery} onChange={(event)=>setSearchQuery(event.target.value)}/>
         <img src="/search.svg"/>
       </form>
-    </div>
-
   )
 }
