fb24add1a4996ce427e38594fb6b3a8400b29c2e
Gabriel Camargo <grocre@gmail.com>
2021-09-14 20:14:59 -0300

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,12 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
 
 const routes = require("./routes");
+const { extend } = require("joi");
+
 
 app.use(express.json());
 app.use("/api", routes);
