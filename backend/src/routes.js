0943465af133dbc75a7e2908cb5105362c618d25
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-21 22:26:07 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const multer = require('multer');
-const uploadConfig = require('./middleware/imageUpload')
 
 const router = express.Router();
-const upload = multer(uploadConfig);
 
 const awardController = require("./controllers/awardController");
 const crewController = require("./controllers/crewController");
