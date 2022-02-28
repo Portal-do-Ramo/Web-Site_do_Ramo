c7b8e1662e11c798197cfe952f5cacfe08c4fd4a
Cleytonpsf <cleytonpsf@gmail.com>
2022-02-28 17:26:17 -0300

    refactoring crewController and crewService

--- backend/src/controllers/crewController.js
+++ backend/src/controllers/crewController.js
@@ -1,30 +1,24 @@
-const knex = require('../database');
-const {v4} = require('uuid');
+const crewService = require("../services/crewService");
 
 module.exports = {
 
     /* O carrossel precisa de dinamismo na lógica do Back-end, a ideia é colocar um argumento obrigatório para conseguir realizar a troca das informações de acordo com a equipe escolhida no carrossel */
 
    async index(req, res) {
-		let crews = await knex('crews').select();
+        let crews = await crewService.index();
         return res.status(200).json(crews);
     },
 
 	async show(req,res) {
 		let {id} = req.params;
-		let crew = await knex("crews").select().where({id});
+		let crew = await crewService.show(id);
 		return res.status(200).json(crew);
 	},
 
     async create(req, res) {
         let { name, about, image } = req.body;
         try{
-            await knex("crews").insert({
-                id: v4(),
-                name, 
-                about,
-				image
-            });
+            await crewService.create(name, about, image);
             return res.status(201).json({"message": "Equipe criada!!!"});
         } catch(err){
             return res.status(422).json({"message": err.message});
@@ -34,7 +28,7 @@ module.exports = {
     async update(req, res) {
 		let { id, crew } = req.body;
 		try {
-			await knex("crew").update(crew).select({id});
+            await crewService.update(id, crew);
 			return res.status(200).json({"message": "Equipe atualizada!!"});
 		} catch(err){
 			return res.status(405).json({"message": err.message});
@@ -44,13 +38,16 @@ module.exports = {
 	async delete(req, res) {
 		let { crew } = req.body;
 		try {
-			let confirmation = await knex("crews").where({"name": crew}).delete();
-            if(confirmation > 1){
+			let confirmation = await crewService.delete(crew);
+            if(confirmation > 0) {
                 return res.status(200).json({"message": "Equipes deletadas"});
-            } 
-            return res.status(200).json({"message": "Equipes deletadas"});
+            }
+            else{
+                return res.status(202).json({"message": "Equipe não existente"});
+            }
+            
 		} catch(err) {
 			return res.status(405).json({"message": err.message});
 		}
-	}
+	} 
 }
---
+++ backend/src/services/crewService.js
@@ -0,0 +1,33 @@
+const knex = require('../database');
+const {v4} = require('uuid');
+
+module.exports = {
+    async index() {
+        let crews = await knex('crews').select();
+        return crews;
+    },
+
+    async show(id) {
+        let crew = await knex("crews").select().where({id});
+        return crew;
+    }, 
+
+    async create(name, about, image) {
+        await knex("crews").insert({
+            id: v4(),
+            name, 
+            about,
+            image
+        });
+    },
+
+    async update(id, crew) {
+        await knex("crew").update(crew).select({id});
+    },
+
+    async delete(crew) {
+        let confirmation = await knex("crews").where({"name": crew}).delete();
+        return confirmation;
+    }
+
+}
