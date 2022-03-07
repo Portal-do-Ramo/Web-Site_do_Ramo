6449b003a5fffad68b55ea5a1cbe49434bbc7403
Cleytonpsf <cleytonpsf@gmail.com>
2022-03-07 16:24:28 -0300

    Update authentication.js

--- backend/src/services/authentication.js
+++ backend/src/services/authentication.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
  
-function authenticate(name, email, role){
-    return jwt.sign({name, email, role}, process.env.TOKEN_HASH, {expiresIn: '48h'});
+function authenticate(name, email){
+    return jwt.sign({name, email}, process.env.TOKEN_HASH, {expiresIn: '48h'}); 
 }
 
 module.exports = authenticate;
