8eb6f727e59ef7672df5fbdb9151e73d203b6bd7
Cleytonpsf <cleytonpsf@gmail.com>
2022-04-02 04:35:24 -0300

    added pseMiddleware

---
+++ backend/src/middleware/pseMiddleware.js
@@ -0,0 +1,24 @@
+const knex = require("../database");
+
+async function pseMiddleware(req, res, next){
+    let response = await knex("pse").select("*");
+    let currentDate = new Date();
+    try{
+        
+        if(response.length == 0){
+            return res.status(403).json("PSE has not been scheduled");    
+        }
+
+        if(currentDate >= response[0].start && currentDate <= response[0].end){
+            return next();
+        }
+
+        return res.status(403).json("PSE has not been scheduled");
+
+    } catch(err){
+        throw new Error(err.message);
+    }
+
+}
+
+module.exports = pseMiddleware;
