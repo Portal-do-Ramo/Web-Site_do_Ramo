fe15c6bbd478737f061730f88d7fae8ae589119f
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-06-04 16:56:52 -0300

    added responsive style to about's page

--- frontend/src/components/ValueContainer/index.js
+++ frontend/src/components/ValueContainer/index.js
@@ -4,8 +4,11 @@ export default function ValueContainer({props}){
     return(
         <div className={styles.valueContainer}>
             <img src={props.img}/>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+
+            <section>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </section>
         </div>
     )
 } 
