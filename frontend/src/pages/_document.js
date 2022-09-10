aecc03448dc39cd304aca18effa59860465c732f
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-09-10 11:26:56 -0300

    unused libraries removed

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -6,17 +6,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            rel="stylesheet"
-            type="text/css"
-            charSet="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
