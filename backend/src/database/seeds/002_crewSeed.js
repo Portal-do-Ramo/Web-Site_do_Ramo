9371c322d7ce4d4e445e89fb7ddc515f9450938c
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-09-24 15:48:45 -0300

    Update 002_crewSeed.js

--- backend/src/database/seeds/002_crewSeed.js
+++ backend/src/database/seeds/002_crewSeed.js
@@ -6,14 +6,14 @@ exports.seed = function(knex) {
     .then(function () {
       
       return knex('crews').insert([
-		    {id: v4(), name: "WIE", about: "Essa é braba", imageURL: "wie_avatar.svg"},
-        {id: v4(), name: "Gestão", about: "depressão e mt sofrimento", imageURL: "gestao_avatar.svg"}, 
-        {id: v4(), name: "Marketing", about: "bo vender uma viagem pra um cara com o dinheiro do auxilio apenas kk", imageURL: "marketing_avatar.svg"},
-        {id: v4(), name: "RocketWolf", about: "ovo lança fuguetin", imageURL: "rocketwolf_avatar.svg"},
-        {id: v4(), name: "SocialWolf", about:"ajuda uns velhinho", imageURL: "socialwolf_avatar.svg"},
-        {id: v4(), name: "WolfBotz", about: "ih bo criar robô que briga kkk", imageURL: "wolfbotz_avatar.svg"},
-        {id: v4(), name: "WolfByte", about: "n ve a luz do sol", imageURL: "wolfbyte_avatar.svg"},
-        {id: v4(), name: "WolfPower", about: "vamo estoca vento", imageURL: "wolfpower_avatar.svg"}
+		    {id: v4(), name: "WIE", about: "", imageURL: "wie_crew_avatar.svg"},
+        {id: v4(), name: "Gestão", about: "", imageURL: "gestao_crew_avatar.svg"}, 
+        {id: v4(), name: "Marketing", about: "", imageURL: "marketing_crew_avatar.svg"},
+        {id: v4(), name: "RocketWolf", about: "", imageURL: "rocketwolf_crew_avatar.svg"},
+        {id: v4(), name: "SocialWolf", about:"", imageURL: "socialwolf_crew_avatar.svg"},
+        {id: v4(), name: "WolfBotz", about: "", imageURL: "wolfbotz_crew_avatar.svg"},
+        {id: v4(), name: "WolfByte", about: "", imageURL: "wolfbyte_crew_avatar.svg"},
+        {id: v4(), name: "WolfPower", about: "", imageURL: "wolfpower_crew_avatar.svg"}
       ]);
     });
 };
