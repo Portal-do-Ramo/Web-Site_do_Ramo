3a5eca23ab2e1f8d82ac15129c7b051e6e6a352d
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-12-02 14:22:30 -0300

    style of projects title fixed

--- frontend/src/components/ProjectDetail/index.js
+++ frontend/src/components/ProjectDetail/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import styles from "./Modal.module.scss";
+import React from "react";
+import styles from "./styles.module.scss";
 
 export function ProjectDetail({ project }) {
 	const urlRegex = /(https?:\/\/[^\s]+)/g;
