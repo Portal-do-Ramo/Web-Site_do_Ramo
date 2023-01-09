de7686c104c2c126e6589154b7bd4e9fe622c5df
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2023-01-09 17:29:55 -0300

    added join us section and remove old sponsors section

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,88 +10,81 @@ import styles from "../styles/index.module.scss";
 
 export default function Home({ crews }) {
 
-  return (
-    <div id={styles.container}>
-      <Header page="inicio">
-        <div className={styles.wolfImageContainer}>
-          <Image 
-            src="/seu_pai.svg"
-            alt="Lobo do Ramo IEEE CEFET-RJ"
-            width={300}
-            height={300}
-          />
-        </div>
-      </Header>
-      
-      <div id={styles.page_container}>
-        <div id={styles.text_container}>
-          <div id={styles.ourStory}>
-            
-            <h1>Nossa História</h1>
+	return (
+		<div className={styles.container}>
+			<Header page="inicio">
+				<div className={styles.wolfImageContainer}>
+					<Image
+						src="/seu_pai.svg"
+						alt="Lobo do Ramo IEEE CEFET-RJ"
+						width={300}
+						height={300}
+					/>
+				</div>
+			</Header>
+			
+			<div className={styles.page_container}>
+				<section className={styles.text_container}>
+					<div className={styles.ourStory}>
+						<h1>Nossa História</h1>
 
-            <p>
-              O Ramo Estudantil IEEE teve um inicio humilde em 2004, mas voltou mais forte em 2016, desde então crescendo passo a passo.
-              Começou focado somente na parte de programação computacional, mas conforme o tempo expandiu as áreas de atuação, criando 
-              equipes para cada uma dessas áreas.
-              <br />
-              <br />
-              Buscamos auxiliar no crescimento das pessoas tanto como profissional como quanto pessoa, com projetos em equipe que 
-              aceitam qualquer nível de experiência, desde que esteja disposto/a a aprender.
-            </p>
-          </div>
+						<p>
+							O Ramo Estudantil IEEE teve um inicio humilde em 2004, mas voltou mais forte em 2016, desde então crescendo passo a passo.
+							Começou focado somente na parte de programação computacional, mas conforme o tempo expandiu as áreas de atuação, criando 
+							equipes para cada uma dessas áreas.
+							<br />
+							<br />
+							Buscamos auxiliar no crescimento das pessoas tanto como profissional como quanto pessoa, com projetos em equipe que 
+							aceitam qualquer nível de experiência, desde que esteja disposto/a a aprender.
+						</p>
+					</div>
 
-          <div id={styles.main_container}>
-            <a href="/sobre" id={styles.button}>
-              <p>Saiba mais</p>
-              <Image src="/right-arrow.svg" width={32} height={32} />
-            </a>
-          </div>
-        </div>
+					<div className={styles.main_container}>
+						<a href="/sobre" className={styles.button}>
+							<p>Saiba mais</p>
+							<Image src="/right-arrow.svg" width={20} height={20} />
+						</a>
+					</div>
+				</section>
 
-        <img src="/Background.png" width="100%" />
+				<section className={styles.crew_content}>
+					<article>
+						<h3>Equipes</h3>
 
-        <div className={styles.crew_content}>
+						<div className={styles.logo_content}>
+						{
+							crews.map((crew, index) => {
+								return (
+									<CrewsCard
+										key={crew.id}
+										index={index} 
+										name={crew.name} 
+										image={crew.imageURL} 
+									/>
+								)
+							})
+						}
+						</div>
+					</article>
+				</section>
+				
+				<section className={styles.join_us}>
+					<article>
+						<img src="/pessoas_quebra_cabeca.svg" alt="ilustração de uma equipe trabalhando junto" />
 
-          <h3>Equipes</h3>
-
-          <section className={styles.logo_content}>
-            {
-              crews.map((crew, index) => {
-                return (
-                  <CrewsCard
-                    key={crew.id}
-                    index={index} 
-                    name={crew.name} 
-                    image={crew.imageURL} 
-                  />
-                )
-              })
-            }
-          </section>
-        </div>
-        
-        <img src="/Background.png" width="100%" className={styles.rotate} />
-        
-        <div className={styles.parcerias}>
-          <h3>Parceiros</h3>
-          <div className={styles.partnerGrid}>
-              <img id={styles.motim} src="/sponsor-logo-motim.png"></img>
-              <img id={styles.tecci} src="/sponsor-logo-TEC_CI_RGB.png"></img>
-              <img id={styles.squair} src="/sponsor-logo-squair.jpg"></img>
-              <img id={styles.u} src="/sponsor-logo-Universinagem.png"></img>
-              <img id={styles.voitto} src="/sponsor-logo-voitto.png"></img>
-              <img id={styles.botz} src="/sponsor-logo-botz-loja.png"></img>
-              <img id={styles.pololu} src="/sponsor-pololu-logo.jpg"></img>
-              <img id={styles.solidworks} src="/sponsor-logo-solidworks.png"></img>
-              <img id={styles.betabit} src="/sponsor-logo-Betabit.png"></img>
-              <img id={styles.altium} src="/sponsor-logo-altium.png"></img>
-              <img id={styles.mlabs} src="/sponsor-logo-mlabs.png"></img>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+						<div>
+							<span>Faça parte da nossa equipe!</span>
+							<p>Se você é estudante de graduação do CEFET-RJ, se inscreva no nosso processo seletivo para trabalhar junto com o maior projeto de extensão do CEFET-RJ. </p>
+							<a href="/PSE" className={styles.button}>
+								<p>Se inscreva já</p>
+							</a>
+						</div>
+					</article>
+				</section>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export const getStaticProps = async () => {
