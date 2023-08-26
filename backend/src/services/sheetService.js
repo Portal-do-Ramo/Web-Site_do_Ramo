30f76984b915fc6797f04826b18ee7cf7c6f75e3
Ana Beatriz <caldas.ana9@gmail.com>
2023-08-26 14:16:13 -0300

    Service de inserção na planilha

--- backend/src/services/sheetService.js
+++ backend/src/services/sheetService.js
@@ -1,8 +1,22 @@
 const db = require("../database/firebase");
 const registerPSE = db.collection("registerPSE");
+const sheetConfig = require("../config/sheetConfig");
 
 module.exports = {
-    async insert(info){
+    async insert(values){
+        const { googleSheets, auth, spreadsheetId } = await sheetConfig.getAuthSheets();
+
+        const row = await googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: "Sheet1",
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: values,
+            }
+        })
+    },
+    async delete(values){
         
     }
 
