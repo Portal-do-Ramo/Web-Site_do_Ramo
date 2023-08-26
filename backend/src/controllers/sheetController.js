8302960e1a11d80f19a5d7ac5cae80bbb8a1ec71
Ana Beatriz <caldas.ana9@gmail.com>
2023-08-26 14:14:27 -0300

    Finalização do service de inserção na planilha

--- backend/src/controllers/sheetController.js
+++ backend/src/controllers/sheetController.js
@@ -1,13 +1,22 @@
-
+const sheetService = require("../services/sheetService")
 
 module.exports = {
-    async insert(req, res){
+    async insert(values){
+        const listValues = Object.values(values.personalInformation).concat(Object.values(values.registrationData))
+        listValues[14] = listValues[14].join(", ")
+
+        const data = {
+            "values": listValues
+        } 
+
         try{
-
+            await sheetService.insert(data);
         } catch (err) {
-            return res.status().json({message: err.message})
+            throw new Error(err.message);
         }
+    },
+
+    async delete(req, res){
+
     }
-
-
 }
