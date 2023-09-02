d435d8feaa8e219e4772f240142a6635ffed3319
Cleytonpsf <cleytonpsf@gmail.com>
2023-09-02 03:22:03 -0300

    Alteração na seed de users

--- backend/src/database/seeds/001_userSeed.js
+++ backend/src/database/seeds/001_userSeed.js
@@ -1,17 +1,19 @@
 const { v4 } = require("uuid");
 const bcrypt = require("bcrypt");
+require('dotenv').config();
 
 exports.seed = async function (knex) {
-	const hash = await bcrypt.hash("123456789", 10);
+	const hash = await bcrypt.hash(process.env.USER_PASSWORD, 10);
 	return knex("users")
 		.del()
 		.then(function () {
 			return knex("users").insert([
 				{
 					id: v4(),
-					name: "Nome Teste",
-					email: "teste@teste.com",
+					name: "Site do Ramo",
+					email: process.env.USER_EMAIL,
 					password: hash,
+					isAdmin: true
 				},
 			]);
 		});
