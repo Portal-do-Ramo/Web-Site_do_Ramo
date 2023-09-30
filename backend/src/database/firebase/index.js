4a1cecace3a6c4e112536905f17753a4ee6226d2
Ana Beatriz <caldas.ana9@gmail.com>
2023-09-30 10:21:33 -0300

    Alteração das credenciais do firestore para admin

--- backend/src/database/firebase/index.js
+++ backend/src/database/firebase/index.js
@@ -1,16 +1,23 @@
-const firebase = require("firebase");
+const admin = require("firebase-admin");
 require('dotenv').config();
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID
-};
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUT_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_CERT_URL,
+  "universe_domain": "googleapis.com"
+}
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
+const db = admin.firestore();
 module.exports = db;
