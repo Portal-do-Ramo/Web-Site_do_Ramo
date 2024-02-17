77e21778e1937f89f82763362f406ff161d5e3dc
tunim73 <antonio.netto973@gmail.com>
2024-02-17 03:07:13 -0300

    feat: deletar coordenador

---
+++ frontend/src/components/DeleteAlertModal/index.js
@@ -0,0 +1,42 @@
+import Modal from "react-modal";
+import styles from "./DeleteAlertModal.module.scss";
+
+export const DeleteAlertModal = ({
+  modalIsOpen,
+  handleCloseModal,
+  title,
+  text,
+  clickFunction,
+}) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={handleCloseModal}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
+      contentLabel="Example Modal"
+      shouldCloseOnEsc={true}
+      style={{ overlay: { zIndex: 10 } }}
+    >
+      <img src="/cancel.svg"></img>
+      <h1>{title}</h1>
+      <p>{text}</p>
+      <div className={styles.rowButton}>
+        <button
+          type="button"
+          className={styles.cancel}
+          onClick={handleCloseModal}
+        >
+          Cancelar
+        </button>
+        <button
+          type="button"
+          className={styles.shutDown}
+          onClick={clickFunction}
+        >
+          Sim, excluir
+        </button>
+      </div>
+    </Modal>
+  );
+};
--- frontend/src/pages/marketing/admin/index.js
+++ frontend/src/pages/marketing/admin/index.js
@@ -1,257 +1,342 @@
 import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import Head from "next/head";
 
-import X from '../../../../public/X.svg';
-import { BiPlusMedical } from 'react-icons/bi';
-import { BsFillGearFill } from 'react-icons/bs';
-
+import X from "../../../../public/X.svg";
+import { BsFillGearFill } from "react-icons/bs";
+import { FaRegTrashAlt } from "react-icons/fa";
 import styles from "./styles.module.scss";
-import api from '../../../services/api'
+import api from "../../../services/api";
 
-import MarketingNavBar from "../../../components/MarketingNavBar"
+import MarketingNavBar from "../../../components/MarketingNavBar";
 import MarketingMenuRoutes from "../../../components/MarketingMenuRoutes";
 
 import { AuthContext } from "../../../contexts/AuthContext";
 import Modal from "react-modal";
 import Image from "next/image";
+import { DeleteAlertModal } from "../../../components/DeleteAlertModal";
 
-export default function admin({crews}){
-	const router = useRouter();
+export default function admin({ crews }) {
+  const router = useRouter();
 
-		const { user, isAuthenticated } = useContext(AuthContext);
-    const [isLoading, setIsLoading] = useState(true);
-		const [selectedUser, setSelectedUser] = useState(null);
-		const [users, setUsers] = useState([]);
-		const [updatedUser, setUpdatedUser] = useState({
-			name: "",
-			password: "",
-			crew:"",
-		});
+  const { user, isAuthenticated } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [updatedUser, setUpdatedUser] = useState({
+    name: "",
+    password: "",
+    crew: "",
+  });
+  const [modalDeleteIsOpen, setModalDeleteIsOpen] = useState(false);
+  const [modalUpdateIsOpen, setModalUpdateIsOpen] = useState(false);
 
-
-    useEffect(() => {
-			if (isAuthenticated) {
-				if (user === null) {
-					router.push("/login");
-				} else if(!user.isAdmin){
-					router.push("/marketing");
-				} else {
-					setIsLoading(false);
-				}
-			}
-    }, [user, isAuthenticated]);
-
-		useEffect(() => {
-			const fetchUsers = async () => {
-				try {
-					const { data } = await api.get("/users");
-					setUsers(data.users);
-					
-				} catch (error) {
-					router.push("/marketing");
-				}
-			};
-		
-			if (isAuthenticated && user !== null) {
-				fetchUsers();
-			}
-		}, [isAuthenticated, user]);
-		
-		function focusInput(inputId) {
-			const input = document.getElementById(inputId);
-			input.focus();
-		}   
-	  const handleOpenModal = (user) => {
-			setSelectedUser(user);
-			setUpdatedUser({ ...user });
-		};
-	
-		const handleCloseModal = () => {
-			setSelectedUser(null);
-		};
-
-		const findCrewById = (userCrewId) => {
-			return crews.find((crew) => crew.id === userCrewId);
-		};
-
-		async function handleSaveUserChanges() {
-			try {
-        let requestBody = {
-          name: updatedUser.name.trim(),
-          email: user.email,
-          crew_id: updatedUser.crew_id,
-          isAdmin: user.isAdmin,
-        };
-  
-        if (updatedUser.password) {
-          requestBody.password = updatedUser.password.trim();
-        }
-  
-        await api.patch(`/user/${selectedUser.id}`, requestBody);
-        router.reload();
-      } catch (error) {
-        console.error(error);
+  useEffect(() => {
+    if (isAuthenticated) {
+      if (user === null) {
+        router.push("/login");
+      } else if (!user.isAdmin) {
+        router.push("/marketing");
+      } else {
+        setIsLoading(false);
       }
-		}
+    }
+  }, [user, isAuthenticated]);
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await api.get("/users");
+        setUsers(data.users);
+      } catch (error) {
+        router.push("/marketing");
+      }
+    };
 
-	const countSuperUsers = users.filter(user => user.isAdmin).length
-	const countCoordinators = users.length - countSuperUsers
-	const DeleteCoordinator = ()_ => {}
-		
-	if (isLoading) {
-        return ( <></> )
-    } else {
+    if (isAuthenticated && user !== null) {
+      fetchUsers();
+    }
+  }, [isAuthenticated, user]);
 
-		return (
-		  <div className={styles.all}>
-			<Head>
-			  <title>Marketing - Admin | IEEE CEFET-RJ</title>
-			</Head>
-	  
-			<MarketingNavBar page="admin" user={user ? user : null} />
-	  
-			<div className={styles.pageContent}>
-				<div className={styles.content}>
-					<MarketingMenuRoutes routesName={`Administradores`} routes={`admin`}/>
-					<div className={styles.row}>
-						<div className={styles.text}>
-							<h1>Lista de Administradores</h1>
-							<p>
-								{countCoordinators > 1 ? `${countCoordinators} coordenadores e` : `1 coordenador e`}
-								 {countSuperUsers > 1 ? ` ${countSuperUsers} administradores` : ` 1 administrador`}
-							</p>
-						</div>
-					</div>
-	  
-					<div className={styles.usersList}>
-						  {users.map((user) => {
-								const nameURL = `https://ui-avatars.com/api/?name=${user ? user.name : "Unknown"}`
-								const userCrew = findCrewById(user.crew_id);
+  function focusInput(inputId) {
+    const input = document.getElementById(inputId);
+    input.focus();
+  }
+  const handleOpenModalUpdate = (user) => {
+    setSelectedUser(user);
+    setUpdatedUser({ ...user });
+    setModalUpdateIsOpen(true);
+  };
 
-								return (
-									<div key={user.id} 
-										className={styles.userRow}
-										style={user.isAdmin ? { backgroundColor: 'rgba(13, 95, 170, 0.2)' } : {}}
-									>
-										<div className={styles.name}>
+  const handleOpenModalDelete = (user) => {
+    setSelectedUser(user);
+    setModalDeleteIsOpen(true);
+  };
 
-											{user.isAdmin ? (
-												<img src="/Ramo_logo.svg" className={styles.userImage} />
-											) : (
-												userCrew ? (
-													<img src={userCrew.imageURL} className={styles.userImage} />
-												) : (
-													<img src={nameURL} className={styles.userImage} />
-												)
-											)}
-											<h2>{user.name}</h2>
-										</div>
-										
-										<span
-											
-											className={user.isAdmin ?  styles.adminGearConfig: styles.gearConfig}
-											onClick={()=>handleOpenModal(user)}><BsFillGearFill/>
-										</span>
-										{selectedUser && selectedUser.id === user.id && (
-												
-												<Modal overlayClassName={styles.overlay} isOpen={true} onRequestClose={handleCloseModal} className={styles.modal}>
-													<div >
-														<div className={styles.modalHeader}>
-														{user.isAdmin ? (
-																<img src="/Ramo_logo.svg" className={styles.userImage} />
-															) : (
-																userCrew ? (
-																	<img src={userCrew.imageURL} className={styles.userImage} />
-																) : (
-																	<img src={nameURL} className={styles.userImage} />
-																)
-															)}
-															<span onClick={handleCloseModal}>
-																<Image src={X} width={20} height={20} />
-															</span>
-														</div>
-														<div className={styles.inputContainer}>
-															<div>
-																<h3>Nome:</h3>
-																<input
-																 type="text"
-																 value={updatedUser.name}
-																	onChange={(e) =>
-																		setUpdatedUser({ ...updatedUser, name: e.target.value })
-																	}
-																/>
-															</div>
-															<div>
-																<h3>E-mail:</h3>
-																<input
-																 type="text"
-																 className={styles.emailInput}
-																 disabled
-																 placeholder={updatedUser.email}
-															 />
-															</div>
-														{updatedUser.isAdmin !== true && (
-																<div onClick={() => focusInput("crewSelector")}>
-																	<h3>Equipe:</h3>
-																	<select
-	
-																			id="crewSelector"
-																			onChange={(e) => setUpdatedUser({ ...updatedUser, crew_id: e.target.value })}// 
-																			value={updatedUser.crew_id}
-																			style={{ color: updatedUser.crew === "" ? "#9A9A9A" : "" }}
-																	>
-																			<option value="" disabled>Selecione uma equipe</option>
-																			{crews.map(crew => (
-																					<option key={crew.id} value={crew.id} style={{ color: "black"}}>
-																							{crew.name}
-																					</option>
-																			))}
-																	</select>
-																</div>
-															)}	
+  const handleCloseModal = () => {
+    setSelectedUser(null);
+    setModalUpdateIsOpen(false);
+    setModalDeleteIsOpen(false);
+  };
 
-															
+  const findCrewById = (userCrewId) => {
+    return crews.find((crew) => crew.id === userCrewId);
+  };
 
-															<div>
-																<h3>Senha:</h3>
-																<input 
-																	type="password" 
-																	value={updatedUser.password || ""}
-																	onChange={(e) =>
-																		setUpdatedUser({ ...updatedUser, password: e.target.value })
-																	}
-																	placeholder="*********"
-																/>
-															</div>
-														</div>
-														<div className={styles.buttonsBox}>
-															<button className={styles.Create} type="submit" onClick={handleSaveUserChanges}>Salvar alterações</button>
-														</div>
-													</div>
-												</Modal>
-											)
-										}
+  async function handleSaveUserChanges() {
+    try {
+      let requestBody = {
+        name: updatedUser.name.trim(),
+        email: user.email,
+        crew_id: updatedUser.crew_id,
+        isAdmin: user.isAdmin,
+      };
 
-									</div>
-								)
-						  })}
-					</div>
-				</div>
-			</div>
-		  </div>
-		) 
-	}
+      if (updatedUser.password) {
+        requestBody.password = updatedUser.password.trim();
+      }
+
+      await api.patch(`/user/${selectedUser.id}`, requestBody);
+      router.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const countSuperUsers = users.filter((user) => user.isAdmin).length;
+  const countCoordinators = users.length - countSuperUsers;
+
+  async function handleDeleteCoord() {
+    try {
+      await api.delete(`/user/${selectedUser.id}`);
+      router.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (isLoading) {
+    return <></>;
+  } else {
+    return (
+      <div className={styles.all}>
+        <Head>
+          <title>Marketing - Admin | IEEE CEFET-RJ</title>
+        </Head>
+
+        <MarketingNavBar page="admin" user={user ? user : null} />
+
+        <div className={styles.pageContent}>
+          <div className={styles.content}>
+            <MarketingMenuRoutes
+              routesName={`Administradores`}
+              routes={`admin`}
+            />
+            <div className={styles.row}>
+              <div className={styles.text}>
+                <h1>Lista de Administradores</h1>
+                <p>
+                  {countCoordinators > 1
+                    ? `${countCoordinators} coordenadores e`
+                    : `1 coordenador e`}
+                  {countSuperUsers > 1
+                    ? ` ${countSuperUsers} administradores`
+                    : ` 1 administrador`}
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.usersList}>
+              {users.map((user) => {
+                const nameURL = `https://ui-avatars.com/api/?name=${
+                  user ? user.name : "Unknown"
+                }`;
+                const userCrew = findCrewById(user.crew_id);
+
+                return (
+                  <div
+                    key={user.id}
+                    className={styles.userRow}
+                    style={
+                      user.isAdmin
+                        ? { backgroundColor: "rgba(13, 95, 170, 0.2)" }
+                        : {}
+                    }
+                  >
+                    <div className={styles.name}>
+                      {user.isAdmin ? (
+                        <img
+                          src="/Ramo_logo.svg"
+                          className={styles.userImage}
+                        />
+                      ) : userCrew ? (
+                        <img
+                          src={userCrew.imageURL}
+                          className={styles.userImage}
+                        />
+                      ) : (
+                        <img src={nameURL} className={styles.userImage} />
+                      )}
+                      <h2>{user.name}</h2>
+                    </div>
+
+                    <div className={styles.options}>
+                      <span
+                        className={
+                          user.isAdmin
+                            ? styles.adminGearConfig
+                            : styles.gearConfig
+                        }
+                        onClick={() => handleOpenModalUpdate(user)}
+                      >
+                        <BsFillGearFill />
+                      </span>
+                      {!user.isAdmin && (
+                        <>
+                          <span
+                            className={styles.deleteButtonCoord}
+                            onClick={() => handleOpenModalDelete(user)}
+                          >
+                            <FaRegTrashAlt />
+                          </span>
+                          <DeleteAlertModal
+                            clickFunction={handleDeleteCoord}
+                            handleCloseModal={handleCloseModal}
+                            id={user.id}
+                            text={
+                              "Tem certeza que você deseja excluir este coordenador?"
+                            }
+                            title={"Excluir coordenador"}
+                            modalIsOpen={modalDeleteIsOpen}
+                          />
+                        </>
+                      )}
+                    </div>
+
+                    <Modal
+                      overlayClassName={styles.overlay}
+                      isOpen={modalUpdateIsOpen}
+                      onRequestClose={handleCloseModal}
+                      className={styles.modal}
+                    >
+                      <div>
+                        <div className={styles.modalHeader}>
+                          {user.isAdmin ? (
+                            <img
+                              src="/Ramo_logo.svg"
+                              className={styles.userImage}
+                            />
+                          ) : userCrew ? (
+                            <img
+                              src={userCrew.imageURL}
+                              className={styles.userImage}
+                            />
+                          ) : (
+                            <img src={nameURL} className={styles.userImage} />
+                          )}
+                          <span onClick={handleCloseModal}>
+                            <Image src={X} width={20} height={20} />
+                          </span>
+                        </div>
+                        <div className={styles.inputContainer}>
+                          <div>
+                            <h3>Nome:</h3>
+                            <input
+                              type="text"
+                              value={updatedUser.name}
+                              onChange={(e) =>
+                                setUpdatedUser({
+                                  ...updatedUser,
+                                  name: e.target.value,
+                                })
+                              }
+                            />
+                          </div>
+                          <div>
+                            <h3>E-mail:</h3>
+                            <input
+                              type="text"
+                              className={styles.emailInput}
+                              disabled
+                              placeholder={updatedUser.email}
+                            />
+                          </div>
+                          {updatedUser.isAdmin !== true && (
+                            <div onClick={() => focusInput("crewSelector")}>
+                              <h3>Equipe:</h3>
+                              <select
+                                id="crewSelector"
+                                onChange={(e) =>
+                                  setUpdatedUser({
+                                    ...updatedUser,
+                                    crew_id: e.target.value,
+                                  })
+                                } //
+                                value={updatedUser.crew_id}
+                                style={{
+                                  color:
+                                    updatedUser.crew === "" ? "#9A9A9A" : "",
+                                }}
+                              >
+                                <option value="" disabled>
+                                  Selecione uma equipe
+                                </option>
+                                {crews.map((crew) => (
+                                  <option
+                                    key={crew.id}
+                                    value={crew.id}
+                                    style={{ color: "black" }}
+                                  >
+                                    {crew.name}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                          )}
+
+                          <div>
+                            <h3>Senha:</h3>
+                            <input
+                              type="password"
+                              value={updatedUser.password || ""}
+                              onChange={(e) =>
+                                setUpdatedUser({
+                                  ...updatedUser,
+                                  password: e.target.value,
+                                })
+                              }
+                              placeholder="*********"
+                            />
+                          </div>
+                        </div>
+                        <div className={styles.buttonsBox}>
+                          <button
+                            className={styles.Create}
+                            type="submit"
+                            onClick={handleSaveUserChanges}
+                          >
+                            Salvar alterações
+                          </button>
+                        </div>
+                      </div>
+                    </Modal>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export const getServerSideProps = async () => {
-  let {data : crews} = await api.get("/crews");
+  let { data: crews } = await api.get("/crews");
 
   return {
     props: {
-      crews
-    }
-  }
-}
+      crews,
+    },
+  };
+};
