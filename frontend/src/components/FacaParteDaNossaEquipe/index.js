311e02efc6abc8c9b9ac549ed95d23a283060d13
tunim73 <antonio.netto973@gmail.com>
2024-02-21 13:07:56 -0300

    add FacaParteDaNossaEquipe

---
+++ frontend/src/components/FacaParteDaNossaEquipe/index.js
@@ -0,0 +1,66 @@
+import styles from "./FacaParteDaNossaEquipe.module.scss";
+import { useRouter } from "next/router";
+
+export const FacaParteDaNossaEquipe = ({ havePSE }) => {
+  const router = useRouter();
+
+  const handlerButton = () => {
+    router.push("/PSE");
+  };
+
+  return (
+    <>
+      <section className={styles.join_us}>
+        <article>
+          <img
+            src="/pessoas_quebra_cabeca.svg"
+            alt="ilustração de uma equipe trabalhando junto"
+          />
+          {havePSE ? (
+            <div>
+              <span>Faça parte da nossa equipe!</span>
+              <p>
+                Se você é estudante de graduação do CEFET-RJ, se inscreva no
+                nosso processo seletivo para trabalhar junto com o maior projeto
+                de extensão do CEFET-RJ.{" "}
+              </p>
+              <button onClick={handlerButton}>Se inscreva já</button>
+            </div>
+          ) : (
+            <div>
+              <span>Faça parte da nossa equipe!</span>
+              <p>
+                O processo seletivo abrirá em breve, esperamos te ver por aqui
+                novamente!
+              </p>
+              <button onClick={handlerButton} disabled>
+                Em breve
+              </button>
+            </div>
+          )}
+        </article>
+      </section>
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  let havePSE = false;
+  try {
+    let { data: resData } = await api.get("/pse");
+
+    if (!isBefore(new Date(), new Date(resData.start))) {
+      havePSE = false;
+    } else {
+      havePSE = true;
+    }
+  } catch (error) {
+    havePSE = false;
+  }
+
+  return {
+    props: {
+      havePSE,
+    },
+  };
+};
