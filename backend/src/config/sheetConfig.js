5ca4ae3679595e421cc8b195b59a941345cebb0a
Cleytonpsf <cleytonpsf@gmail.com>
2024-07-05 20:55:17 -0300

    configuração das variáveis de ambiente privadas

--- backend/src/config/sheetConfig.js
+++ backend/src/config/sheetConfig.js
@@ -6,7 +6,7 @@ const sheetCredentials = {
 	'type': 'service_account',
 	'project_id': process.env.SHEET_PROJECT_ID,
 	'private_key_id': process.env.SHEET_PRIVATE_KEY_ID,
-	'private_key': process.env.SHEET_PRIVATE_KEY,
+	'private_key': process.env.SHEET_PRIVATE_KEY.replace(/\n/gm, "\n"),
 	'client_email': process.env.SHEET_CLIENT_EMAIL,
 	'client_id': process.env.SHEET_CLIENT_ID,
 	'auth_uri': process.env.AUTH_URI,
