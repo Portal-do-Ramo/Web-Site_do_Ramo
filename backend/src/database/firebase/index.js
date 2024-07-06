6a1abb39b864c99d606caeb1d64da98b2177c38b
Cleytonpsf <cleytonpsf@gmail.com>
2024-07-05 21:18:20 -0300

    update: configuração das variáveis de ambiente privadas

--- backend/src/database/firebase/index.js
+++ backend/src/database/firebase/index.js
@@ -5,7 +5,7 @@ const serviceAccount = {
 	'type': 'service_account',
 	'project_id': process.env.FIREBASE_PROJECT_ID,
 	'private_key_id': process.env.FIREBASE_KEY_ID,
-	'private_key': process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n"),
+	'private_key': process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	'client_email': process.env.FIREBASE_CLIENT_EMAIL,
 	'client_id': process.env.FIREBASE_CLIENT_ID,
 	'auth_uri': process.env.AUTH_URI,
