c9e68729a4e523f04aeba2b09816c3468d413851
Cleytonpsf <cleytonpsf@gmail.com>
2024-07-11 21:26:29 -0300

    configuração para usar o bd em produção
    se estiver usando em desenvolvimento com BD local deve-se comentar a parte o ssl

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 module.exports = {
-
 	development: {
 		client: 'pg',
 
 		connection: {
-			database: process.env.DATABASE_NAME, 
-			user: process.env.DATABASE_USERNAME, 
+			host: process.env.DATABASE_HOST || '127.0.0.1',
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USERNAME,
 			password: process.env.DATABASE_PASSWORD,
-			port: process.env.DATABASE_PORT
-		}, 
+			port: process.env.DATABASE_PORT,
+			ssl: {
+				rejectUnauthorized: false
+			}
+		},
 		migrations: {
-			tableName: 'migrations', 
+			tableName: 'migrations',
 			directory: `${__dirname}/src/database/migrations`
-		}, 
+		},
 		seeds: {
 			directory: `${__dirname}/src/database/seeds`
 		}
