c2b354a331005aaf7d863a41dd03ec4336533382
Arthur Duarte <duartearthur42@gmail.com>
2024-09-24 21:53:37 -0300

    fix: ssl remove comment

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -9,9 +9,9 @@ module.exports = {
 			user: process.env.DATABASE_USERNAME,
 			password: process.env.DATABASE_PASSWORD,
 			port: process.env.DATABASE_PORT,
-			// ssl: {
-			// 	rejectUnauthorized: false
-			// }
+			ssl: {
+				rejectUnauthorized: false
+			}
 		},
 		migrations: {
 			tableName: 'migrations',
