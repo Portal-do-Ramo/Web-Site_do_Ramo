2c22c8ef4e60234088188f25b527ae59ab82a833
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-12-27 20:49:08 -0300

    feat: inicio da implementação da verificação de email

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendEmail (recipientEmail) {
+	try {
+		const senderEmail = 'sitedoramo@gmail.com';
+
+		const message = {
+			to: recipientEmail,
+			from: senderEmail,
+			subject: 'Verificação de Contato',
+			html: `
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+          <head>
+            <meta charset="UTF-8">
+            <title>Verificação de E-mail Concluída</title>
+          </head>
+          <body>
+            <div>
+              <h1>Verificação de E-mail Concluída</h1>
+              <p>Olá,</p>
+              <p>Este é um e-mail de confirmação para verificar o seu endereço de e-mail.</p>
+              <p>Ficamos felizes em informar que a verificação foi concluída com sucesso! Agora, sabemos que podemos entrar em contato com você sempre que necessário.</p>
+			  <p>Desejamos um bom processo seletivo!</p>
+              <p>Atenciosamente,<br>Equipe do Site do Ramo</p>
+            </div>
+          </body>
+        </html>
+      `
+		};
+
+		await sgMail.send(message);
+	} catch (error) {
+		throw new Error(error.message);
+	}
+}
+
+(async () => {
+	const recipientEmail = 'marques.a1n1a@gmail.com'; 
+	await sendEmail(recipientEmail);
+})();
+  
+module.exports = sendEmail;
